8bce6f05ec27a8aa56ac9ff5cee4f32cb1a11fa3
yhiroky16 <h.yoshida@oplan.co.jp>
2019-03-03 11:51:42 +0900

    サンプルコミット

---
+++ app/assets/javascripts/channels/chat.js
@@ -0,0 +1,25 @@
+App.chat = App.cable.subscriptions.create("ChatChannel", {
+  connected: function() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected: function() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received: function(data) {
+    // Called when there's incoming data on the websocket for this channel
+    console.log('receive');
+    return $('#chat-index').append('<li>' + data.message + '</li>');
+  },
+
+  post: function(message) {
+    return this.perform('post', { message: message });
+  }
+}, $(document).on('keypress', '[data-behavior~=chat_post]', function(event) {
+  if (event.keyCode === 13) {
+    var chatForm = $('#chat-form');
+    App.chat.post(chatForm.val());
+    return
+  }
+}));
